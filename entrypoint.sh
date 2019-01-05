15f346cabf0ac077e8f4c0c47d822acd6415bf54
Justin <justingosan@users.noreply.github.com>
2019-01-06 02:02:30 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@ cd /chisel
 wget -O chisel.gz  https://github.com/jpillora/chisel/releases/download/${VER}/chisel_linux_amd64.gz
 gzip -d chisel.gz 
 chmod +x chisel
-./chisel server --port $PORT --auth $AUTH --socks5
+./chisel server --port $PORT --auth $AUTH --reverse --socks5
